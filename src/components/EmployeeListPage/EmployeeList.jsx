04cfdf2bc19b95e52b6548d5eb0d28f7a6baf6da
sraj1820 <sraj1820@gmail.com>
2022-07-01 14:14:28 -0400

    fix card import from bootstrap

--- src/components/EmployeeListPage/EmployeeList.jsx
+++ src/components/EmployeeListPage/EmployeeList.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import Card from 'react-bootstrap/card'
+import Card from 'react-bootstrap/Card';
 import NewEmployee from '../NewEmployeeForm/NewEmployee'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import '../EmployeeListPage/employeeList.css'
